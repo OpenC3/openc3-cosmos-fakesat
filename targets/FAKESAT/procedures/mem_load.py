4ea6f7558167c4e973f85f3fedee77fe13b096eb
Jason Thomas <jason@openc3.com>
2024-03-31 08:58:21 -0600

    Add ops_suite.py

--- targets/FAKESAT/procedures/mem_load.py
+++ targets/FAKESAT/procedures/mem_load.py
@@ -4,7 +4,7 @@ cmd(f"FAKESAT TABLE_LOAD with DATA {data}")
 cmd("FAKESAT", "TABLE_LOAD", {"DATA": data})
 wait(2)  # Allow telemetry to change
 # Can't use check for binary data so we grab the data
-# and check simply using Ruby comparison
+# and check simply using comparison
 block = tlm("FAKESAT HEALTH_STATUS TABLE_DATA")
 if data != block:
     raise RuntimeError("TABLE_DATA not updated correctly!")
